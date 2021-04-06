b033540a62608ddcd096152a57ed81dd9bd72021
Luiz Fernando <lfaires@gmail.com>
2021-04-06 18:57:27 -0300

    prompt recebe apenas valores pares

--- Script/script.js
+++ Script/script.js
@@ -1,1 +1,8 @@
-//const numCards = prompt("Com quantas cartas deseja jogar?");//
+const numCards = prompt("Com quantas cartas deseja jogar?");
+let remainder = parseInt(numCards) % 2;
+
+
+while (remainder !== 0) {
+    const numCards = prompt("Com quantas cartas deseja jogar?");
+    remainder = parseInt(numCards) % 2;
+}
