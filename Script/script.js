73d04d88e827ad25502a05566f84f588dcb0ce2f
Luiz Fernando <lfaires@gmail.com>
2021-04-06 22:49:05 -0300

    adição da função turnCard para a carta virar após o click

--- Script/script.js
+++ Script/script.js
@@ -5,9 +5,17 @@ while (remainder !== 0 || numCards >14 || numCards <4){
     numCards = parseInt(prompt("Com quantas cartas deseja jogar?"));
     remainder = parseInt(numCards) % 2;
 }
-const listCard = "<li class=\"card\"><img src=\"../Images/front.png\"></li>";
+const listCard = "<li class=\"card\" onclick=\"turnCard()\"><img src=\"../Images/front.png\"></li>";
 const numberOfList = document.querySelector(".container")
 let i = 0;
 for(let i=0;i<numCards;i++){
+    const listCard = "<li class=\"card\" onclick=\'turnCard()'\><img src=\"../Images/front.png\"></li>";
     numberOfList.innerHTML += listCard
 }
+
+function turnCard(){
+const imgCard = document.querySelector(".card")
+imgCard.innerHTML = "<img src=\"../Images/bobrossparrot.gif\">"
+
+}
+
