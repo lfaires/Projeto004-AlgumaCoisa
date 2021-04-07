e687b143cd3176f32989270770367aff5c1b7422
Luiz Fernando <lfaires@gmail.com>
2021-04-06 22:28:37 -0300

    Troquei a img front de background para <img>, adicionei flexbox e border-radius nos cards

--- Script/script.js
+++ Script/script.js
@@ -5,7 +5,7 @@ while (remainder !== 0 || numCards >14 || numCards <4){
     numCards = parseInt(prompt("Com quantas cartas deseja jogar?"));
     remainder = parseInt(numCards) % 2;
 }
-const listCard = "<li class=\"card\"></li>";
+const listCard = "<li class=\"card\"><img src=\"../Images/front.png\"></li>";
 const numberOfList = document.querySelector(".container")
 let i = 0;
 for(let i=0;i<numCards;i++){
