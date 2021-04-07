c821687760a5c4ac55130dfbe94dad227a4935a4
Luiz Fernando <lfaires@gmail.com>
2021-04-06 22:10:22 -0300

    JS para inserir na tela a quantidade de cartas desejadas

--- Script/script.js
+++ Script/script.js
@@ -1,7 +1,13 @@
 let numCards = parseInt(prompt("Com quantas cartas deseja jogar?"));
 let remainder = numCards % 2;
 
-while (remainder%2 !== 0 || numCards >14 || numCards <4){
+while (remainder !== 0 || numCards >14 || numCards <4){
     numCards = parseInt(prompt("Com quantas cartas deseja jogar?"));
     remainder = parseInt(numCards) % 2;
+}
+const listCard = "<li class=\"card\"></li>";
+const numberOfList = document.querySelector(".container")
+let i = 0;
+for(let i=0;i<numCards;i++){
+    numberOfList.innerHTML += listCard
 }
