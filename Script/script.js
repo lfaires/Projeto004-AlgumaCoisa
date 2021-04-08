1e9e816761387e4ea9128a4474ccbb9e73deed4e
Luiz Fernando <lfaires@gmail.com>
2021-04-08 09:45:07 -0300

    implementei a função para as cartas virarem, o onclick e renomeei classes

--- Script/script.js
+++ Script/script.js
@@ -5,17 +5,15 @@ while (remainder !== 0 || numCards >14 || numCards <4){
     numCards = parseInt(prompt("Com quantas cartas deseja jogar?"));
     remainder = parseInt(numCards) % 2;
 }
-const listCard = "<li class=\"card\" onclick=\"turnCard()\"><img src=\"../Images/front.png\"></li>";
-const numberOfList = document.querySelector(".container")
+const listCard = "<li class=\"card\" onclick=\"flipCard(this)\"><img src=\"../Images/front.png\"></li>";
+const numberOfList = document.querySelector(".game")
 let i = 0;
 for(let i=0;i<numCards;i++){
-    const listCard = "<li class=\"card\" onclick=\'turnCard()'\><img src=\"../Images/front.png\"></li>";
+    const listCard = "<li class=\"card\" onclick=\'flipCard(this)'\><img src=\"../Images/front.png\"></li>";
     numberOfList.innerHTML += listCard
 }
 
-function turnCard(){
-const imgCard = document.querySelector(".card")
-imgCard.innerHTML = "<img src=\"../Images/bobrossparrot.gif\">"
-
+function flipCard(flip) {
+    flip.classList.toggle("flip")
 }
 
